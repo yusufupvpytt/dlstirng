b0a5db2830bc7cca1f230641bec719dac3eed08a
yusufupvpytt <49607618+yusufupvpytt@users.noreply.github.com>
2020-07-07 21:48:16 +0300

    Update libary.lua

--- libary.lua
+++ libary.lua
@@ -4,7 +4,6 @@ local defaults; do
         local mouse        = game:GetService("Players").LocalPlayer:GetMouse();
         local inputService = game:GetService('UserInputService');
         local heartbeat    = game:GetService("RunService").Heartbeat;
-        -- // credits to Ririchi / Inori for this cute drag function :)
         function dragger.new(frame)
             local s, event = pcall(function()
                 return frame.MouseEnter
